05f9baded5e1a0c59276cb4c78a7aabb5c502982
Melissa <melissa.scmi@gmail.com>
2021-08-11 11:24:50 -0700

    WIP: trying to fix subtract functions; number 3 returning NAN

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-function subtract(number1, number2) {
-  return number1 - number2;
+function subtract(number3, number4) {
+  return number3 - number4;
 }
 
 function multiply(number1, number2) {
@@ -16,10 +16,21 @@ function divide(number1, number2) {
 
 $(document).ready(function() {
   $("form#add").submit(function(event) {
+    console.log("script executing!")
     event.preventDefault();
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
     $("#output").text(result);
+  });
+});
+$(document).ready(function() {
+  $("form#subtract").submit(function(event) {
+    console.log("script executing!")
+    event.preventDefault();
+    const number3 = parseInt($("#subtract1").val());
+    const number4 = parseInt($("#subtract2").val());
+    const result2 = subtract(number3, number4);
+    $("#output2").text(result2);
   });
 });
